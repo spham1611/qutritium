c54f519de9d2f96819231747f6092f1dc1cd77d8
Son Pham <reachphamhson@gmail.com>
2023-02-22 21:24:33 +0700

    Change the pulse model without using setter + some functions for showing things.

--- src/pulse.py
+++ src/pulse.py
@@ -1,29 +1,30 @@
 """
-Contain pulse class
+Contain pulse class and its example
 """
+from functools import singledispatchmethod
 
 
 class Pulse:
     """
-    Our pulse have 5 distinct parameters for us to control and modify
+    Our pulse have 5 distinct parameters which can be accessed and shown separately
     """
     def __int__(self, frequency, x_amp, sx_amp,
                 beta, mode='01', duration=None):
         """
 
-        :param frequency:
+        :param frequency: Hz
         :param x_amp:
         :param sx_amp:
         :param beta:
-        :param mode:
+        :param mode: There are two modes only 01 and 12
         :param duration:
         :return:
         """
-        self.frequency = frequency
-        self.x_map = x_amp
-        self.sx_amp = sx_amp
-        self.beta = beta
-        self.mode = mode
+        self._frequency = frequency
+        self._x_amp = x_amp
+        self._sx_amp = sx_amp
+        self._beta = beta
+        self._mode = mode
         self._duration = duration
         self._sigma = duration/4 if duration else 0
 
@@ -31,62 +32,25 @@ class Pulse:
     def frequency(self):
         return self._frequency
 
-    @frequency.setter
-    def frequency(self, value):
-        if not 5000 <= value <= 200000:
-            raise ValueError("Invalid range of frequency. Should be between (5000, 200000")
-        self._frequency = value
-
     @property
     def x_amp(self):
         return self._x_amp
-
-    @x_amp.setter
-    def x_amp(self, value):
-        if not 0 <= value <= 200000:
-            raise ValueError("Invalid range of x amplitude. Should be between (0, 200000)")
-        self._x_amp = value
 
     @property
     def sx_amp(self):
         return self._sx_amp
 
-    @sx_amp.setter
-    def sx_amp(self, value):
-        if not 0 <= value <= 200000:
-            raise ValueError("Invalid range of sx amplitude. Should be between (0, 200000)")
-        self._sx_amp = value
-
     @property
     def beta(self):
         return self._beta
-
-    @beta.setter
-    def beta(self, value):
-        if not 0 <= value <=2:
-            raise ValueError("Invalid range of beta amplitude. Should be between (0, 2)")
-        self._beta = value
 
     @property
     def mode(self):
         return self._mode
 
-    @mode.setter
-    def mode(self, value: str):
-        if value == '01':
-            self._mode = '01'
-        elif value == '12':
-            self._mode = '12'
-        else:
-            raise ValueError("Invalid mode of pulse")
-
     @property
     def duration(self):
         return self._duration
-
-    @duration.setter
-    def duration(self, value):
-        self._duration = value
 
     @property
     def sigma(self):
@@ -95,4 +59,14 @@ class Pulse:
     def __str__(self):
         return f"Pulse(f:{self.frequency}, x_amp:{self.x_amp}, sx_amp:{self.sx_amp}" \
                f"beta:{self.beta}, mode:{self.mode}, duration:{self.duration}, sigma:{self.sigma}"
+
+    def show(self):
+        pass
+
+    def save(self):
+        pass
+
+    @staticmethod
+    def convert_to_qiskit_pulse():
+        pass
 
