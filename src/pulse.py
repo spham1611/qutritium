6eeb9a0924ddb4637d3c62e48413a4e48584a71f
Son Pham <reachphamhson@gmail.com>
2023-02-23 23:07:05 +0700

    Document pulse file

--- src/pulse.py
+++ src/pulse.py
@@ -6,10 +6,10 @@ from functools import singledispatchmethod
 
 class Pulse:
     """
-    Our pulse have 5 distinct parameters which can be accessed and shown separately
+    Our pulse have 5 distinct parameters which can be accessed, shown and saved as a plot
     """
     def __int__(self, frequency, x_amp, sx_amp,
-                beta, mode='01', duration=None):
+                beta, mode='01', duration=None) -> None:
         """
 
         :param frequency: Hz
@@ -30,43 +30,75 @@ class Pulse:
 
     @property
     def frequency(self):
+        """
+        Frequency getter
+        :return: Frequency in Hz unit
+        """
         return self._frequency
 
     @property
     def x_amp(self):
+        """
+        X_amp getter
+        :return: x_amp
+        """
         return self._x_amp
 
     @property
     def sx_amp(self):
+        """
+        SX_amp getter
+        :return: sx_amp
+        """
         return self._sx_amp
 
     @property
     def beta(self):
+        """
+        Beta getter
+        :return: Beta
+        """
         return self._beta
 
     @property
-    def mode(self):
+    def mode(self) -> str:
+        """
+        Mode getter
+        :return: mode in string
+        """
         return self._mode
 
     @property
     def duration(self):
+        """
+        Duration getter
+        :return: duration in seconds
+        """
         return self._duration
 
     @property
     def sigma(self):
+        """
+        Sigma getter
+        :return: sigma
+        """
         return self._sigma
 
     def __str__(self):
+        """String representation"""
         return f"Pulse(f:{self.frequency}, x_amp:{self.x_amp}, sx_amp:{self.sx_amp}" \
                f"beta:{self.beta}, mode:{self.mode}, duration:{self.duration}, sigma:{self.sigma}"
 
     def show(self):
+        """Show the plot of pulse"""
         pass
 
     def save(self):
+        """Save pulse plot"""
         pass
 
     @staticmethod
     def convert_to_qiskit_pulse():
+        """Convert to qiskit pulse type"""
         pass
 
