0b60164168e2ef4ea66a2fd7df6f42d25118c062
Son Pham <reachphamhson@gmail.com>
2023-03-02 17:04:48 +0700

    Create rough draft of TR and rough rabi classes

--- src/calibration/rough_rabi.py
+++ src/calibration/rough_rabi.py
@@ -1,9 +1,110 @@
 """Rough rabi techniques"""
+from qiskit.tools import job_monitor
+
+from src.calibration.calibration_utility import g01, g12
+from abc import ABC, abstractmethod
+from qiskit.circuit import Gate, QuantumCircuit
+from qiskit import schedule
+from src.calibration import backend
+import numpy as np
+from src.constant import QUBIT_PARA
 
 
 class Rough_Rabi:
     """
     The class act as provider + regulator for Rough Rabi techniques
     """
-    def __int__(self):
-        pass
+    def __init__(self) -> None:
+        """
+
+        """
+        self._package = list()
+        self.amps = np.linspace(-0.75, 0.75, 100)
+        self._job_id = 0
+        # Standard flow of protocol
+        self.rr_circuit()
+        self.rr_job_monitor()
+
+    @property
+    def job_id(self):
+        return self._job_id
+
+    @abstractmethod
+    def rr_circuit(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def rr_job_monitor(self):
+        raise NotImplementedError
+
+
+class Rough_Rabi01(Rough_Rabi):
+    """
+
+    """
+    def __init__(self) -> None:
+        """
+
+        """
+        super().__init__()
+
+    def rr_circuit(self):
+        """
+
+        :return:
+        """
+        right_theta_01 = Gate(r'$\theta_x^{01}$', 1, [])
+        for a in self.amps:
+            amp01 = QuantumCircuit(1, 1)
+            amp01.append(right_theta_01, [0])
+            amp01.measure(0, 0)
+            amp01.add_calibration(right_theta_01, (0,), g01(a, 0), [])
+            self._package.append(amp01)
+
+    def rr_job_monitor(self):
+        """
+
+        :return:
+        """
+        schedule(self._package[99], backend=backend).draw(backend=backend)
+        amp01_job = backend.run(self._package, meas_level=1, meas_return='avg', shots=2048)
+        self._job_id = amp01_job.job_id()
+        job_monitor(amp01_job)
+
+
+class Rough_Rabi12(Rough_Rabi):
+    """
+
+    """
+    def __init__(self) -> None:
+        """
+
+        """
+        super().__init__()
+
+    def rr_circuit(self) -> None:
+        """
+
+        :return:
+        """
+        theta_12_x = Gate(r'$\theta_x^{(12)}$', 1, [])
+        pi_01_x = Gate(r'$\pi_x^{(01)}$', 1, [])
+
+        for a in self.amps:
+            amp12 = QuantumCircuit(1, 1)
+            amp12.append(pi_01_x, [0])
+            amp12.append(theta_12_x, [0])
+            amp12.measure(0, 0)
+            amp12.add_calibration(pi_01_x, (0,), g01(QUBIT_PARA.PI.value, 0), [])
+            amp12.add_calibration(theta_12_x, (0,), g12(a, 0), [])
+            self._package.append(amp12)
+
+    def rr_job_monitor(self) -> None:
+        """
+
+        :return:
+        """
+        schedule(self._package[0], backend=backend).draw()
+        amp12_job = backend.run(self._package, meas_level=1, meas_return='avg', shots=2 ** 14)
+        self._job_id = amp12_job.job_id()
+        job_monitor(amp12_job)
--- src/calibration/transmission_reflection.py
+++ src/calibration/transmission_reflection.py
@@ -1,11 +1,13 @@
-"""Transmission and reflection techniques"""
+"""Transmission and reflection techniques for 0-1 and 1-2
+In here TR or tr stands for transmission and reflection
+"""
 from abc import ABC, abstractmethod
 from numpy import linspace
-from . import backend
-from ..constant import QUBIT_PARA
-import numpy as np
+from src.calibration import backend
+from src.calibration import QUBIT_VAL, ANHAR
 from qiskit.circuit import Parameter, Gate, QuantumCircuit
 import qiskit.pulse as pulse
+import numpy as np
 
 
 class TR(ABC):
@@ -14,13 +16,14 @@ class TR(ABC):
     """
     def __init__(self, duration=0) -> None:
         """
-
-        :param duration:
+        Some parameters are restricted for design and read-only
+        :param duration: in seconds
         """
         self._duration = duration
         self._freq = 0
         self._sweep_schedule = None
         self._freq_probe = None
+        self._exp_circuits = None
         self.tr_create_pulse()
         self.tr_create_gate()
         self.run_monitor()
@@ -28,17 +31,33 @@ class TR(ABC):
     @property
     def duration(self):
         return self._duration
- 
+
+    @property
+    def freq(self):
+        return self._freq
+
     @abstractmethod
     def tr_create_pulse(self):
+        """
+
+        :return:
+        """
         raise NotImplementedError
 
     @abstractmethod
     def tr_create_gate(self):
+        """
+
+        :return:
+        """
         raise NotImplementedError
 
     @abstractmethod
     def run_monitor(self):
+        """
+
+        :return:
+        """
         raise NotImplementedError
 
 
@@ -51,17 +70,23 @@ class TR_01(TR):
         """
         super().__init__(duration)
 
+    @property
+    def sweep_schedule(self):
+        return self._sweep_schedule
+
     def tr_create_pulse(self):
         """
 
         :return:
         """
-        est_freq = backend.configuration().hamiltonian['vars'][f'wq{QUBIT_PARA.QUBIT}']/(2*np.pi)
+        est_freq = backend.configuration().hamiltonian['vars'][f'wq{QUBIT_VAL}']/(2*np.pi)
         max_freq, min_freq = est_freq + 30 * 1e6, est_freq - 30 * 1e6
         self._freq_probe = linspace(min_freq, max_freq, 100)
         self._freq = Parameter("freq")
+
+        # Setting up pulse properties for 01
         with pulse.build(backend=backend) as sweep_schedule:
-            drive_chan = pulse.drive_channel(qubit=QUBIT_PARA.QUBIT)
+            drive_chan = pulse.drive_channel(qubit=QUBIT_VAL)
             pulse.delay(48, drive_chan)
             pulse.set_frequency(self._freq, drive_chan)
             pulse.play(pulse.Gaussian(
@@ -69,19 +94,25 @@ class TR_01(TR):
                 amp=0.2,
                 sigma=self.duration / 4,
             ), drive_chan)
-        self._sweep_schedule = sweep_schedule
+            self._sweep_schedule = sweep_schedule
+
+        # If the system does not respond -> TimeoutError occurs
+        if not self._sweep_schedule:
+            raise TimeoutError
 
     def tr_create_gate(self):
         """
 
         :return:
         """
-        g01 = Gate("g01", 1, self._freq)
+        g01 = Gate("g01", 1, self.freq)
         qc = QuantumCircuit(1, 1)
         qc.append(g01, [0])
         qc.measure(0, 0)
-        qc.add_calibration(g01, (0, ), self._sweep_schedule, [self._freq])
-        exp_circuits = [qc.assign_parameters({self._freq: f}, inplace=False) for f in self._freq_probe]
+        qc.add_calibration(g01, (0, ), self._sweep_schedule, [self.freq])
+
+        # Get the circuits from assigned frequencies
+        self._exp_circuits = [qc.assign_parameters({self._freq: f}, inplace=False) for f in self._freq_probe]
 
     def run_monitor(self):
         pass
@@ -94,6 +125,9 @@ class TR_12(TR):
 
         :param duration:
         """
+        self._tr_01 = TR_01(duration=duration)
+        self._pulse01_freq = self._tr_01.freq
+        self._pulse01_schedule = self._tr_01.sweep_schedule
         super().__init__(duration)
 
     def tr_create_pulse(self):
@@ -101,20 +135,26 @@ class TR_12(TR):
 
         :return:
         """
-        est_freq = pulse01freq + 1*anhar
+        est_freq = self._pulse01_freq + 1 * ANHAR
         max_freq, min_freq = est_freq + 30 * 1e6, est_freq - 30 * 1e6
         self._freq_probe = linspace(min_freq, max_freq, 100)
         self._freq = Parameter("freq")
+
+        # Setting up pulse properties for 12
         with pulse.build(backend=backend) as sweep_schedule:
-            drive_chan = pulse.drive_channel(qubit=QUBIT_PARA.QUBIT)
+            drive_chan = pulse.drive_channel(qubit=QUBIT_VAL)
             pulse.delay(48, drive_chan)
-            pulse.set_frequency(self._freq, drive_chan)
+            pulse.set_frequency(self.freq, drive_chan)
             pulse.play(pulse.Gaussian(
                 duration=self.duration,  # input
                 amp=0.2,
                 sigma=self.duration / 4,
             ), drive_chan)
-        self._sweep_schedule = sweep_schedule
+            self._sweep_schedule = sweep_schedule
+
+        # If the system does not respond -> TimeoutError occurs
+        if not self._sweep_schedule:
+            raise TimeoutError
 
     def tr_create_gate(self):
         """
@@ -122,15 +162,18 @@ class TR_12(TR):
         :return:
         """
         g01 = Gate("g01", 1, [])
-        g12 = Gate("g12", 1, [self._freq])
+        g12 = Gate("g12", 1, [self.freq])
         qc = QuantumCircuit(1, 1)
         qc.append(g01, [0])
         qc.append(g12, [0])
         qc.measure(0, 0)
-        qc.add_calibration(g01, (0,), pulse01sched, [self._freq])
-        qc.add_calibration(g12, (0,), self._sweep_schedule, [self._freq])
-        exp_circuits = [qc.assign_parameters({self._freq: f}, inplace=False) for f in self._freq_probe]
+        qc.add_calibration(g01, (0,), self._pulse01_schedule, [self.freq])
+        qc.add_calibration(g12, (0,), self._sweep_schedule, [self.freq])
+
+        # Get the circuits from assigned frequencies
+        self._exp_circuits = [qc.assign_parameters({self.freq: f}, inplace=False) for f in self._freq_probe]
 
     def run_monitor(self):
         """"""
         pass
+
