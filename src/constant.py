f633c2072a8730570f57a3c1832d2d18d674f4ee
Son Pham <reachphamhson@gmail.com>
2023-03-21 19:10:35 +0700

    Add constants

--- src/constant.py
+++ src/constant.py
@@ -1,11 +1,20 @@
 # Constants file
 from enum import Enum
-import numpy as np
-from qiskit.circuit import Parameter
 
 
 class QUBIT_PARA(Enum):
-    QUBIT = 0
-    PI = np.pi
-    THETA = Parameter('theta')
-    PHI = Parameter('phi')
+    # Frequency unit
+    KHZ = 1.0e3
+    MHZ = 1.0e6
+    GHZ = 1.0e9
+
+    # Other constants
+    CBIT = 0
+    QUBIT = 6
+    ACQUIRE_ALIGNMENT = 16
+    PULSE_ALIGNMENT = 16
+    GRANULARITY = 16
+    LCM = 16
+
+    # Scale
+    SCALE_FACTOR = 1e-7
