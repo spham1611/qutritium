1a60e6dfef0de48b8d3bf2dc89877c195d67ccfd
Son Pham <reachphamhson@gmail.com>
2023-03-21 19:09:45 +0700

    Add more parameters

--- src/calibration/__init__.py
+++ src/calibration/__init__.py
@@ -1,15 +1,19 @@
 """Import packages and set up IBM backend for calibration"""
 from qiskit import IBMQ
-# from qiskit.tools.monitor import job_monitor
 from src.constant import QUBIT_PARA
+import numpy as np
 
-
+# IBM Config
 if not IBMQ.active_account():
     IBMQ.load_account()
 
 provider = IBMQ.get_provider(hub='ibm-q', group='open', project='main')
-backend = provider.get_backend('ibm_oslo')
+backend = provider.get_backend('ibm_nairobi')
+
+# Constants value coming from the IBM quantum computer. Because those depend
+# on computer, we will not save in the constant file
 QUBIT_VAL = int(QUBIT_PARA.QUBIT.value)
-ANHAR = backend.properties().qubits[QUBIT_VAL][3].value * 1e9
-
-
+ANHAR = backend.properties().qubits[QUBIT_VAL][3].value * QUBIT_PARA.GHZ.value
+DEFAULT_F01 = backend.defaults().qubit_freq_est[QUBIT_VAL]
+DEFAULT_F12 = DEFAULT_F01 + ANHAR
+DRIVE_FREQ = backend.configuration().hamiltonian['vars'][f'wq{QUBIT_VAL}'] / (2 * np.pi)
