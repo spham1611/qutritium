7f1416da76744a465c8f40ab4c36d711c76ff4ef
Son Pham <reachphamhson@gmail.com>
2023-05-05 21:03:35 +0700

    Changes in backend_ibm.py

--- src/backend_ibm.py
+++ src/backend_ibm.py
@@ -1,11 +1,11 @@
 """List all the backend available and assign qubit value"""
-from qiskit.providers import Backend
-from qiskit_ibm_provider import IBMProvider
-from typing import DefaultDict, Tuple
+from qiskit.providers.backend import Backend
+from qiskit_ibm_provider import IBMProvider, IBMBackend
+from typing import DefaultDict, Tuple, List
 from src.constant import QUBIT_PARA
 
 
-class BackEndList(DefaultDict[str, int]):
+class BackEndList(DefaultDict[str, (IBMBackend, int)]):
     """Show the name of backends and map the qubit used for each backend"""
 
     def __init__(self, /, token: str = '') -> None:
@@ -15,8 +15,10 @@ class BackEndList(DefaultDict[str, int]):
         super().__init__()
         # IBM Config -> activate account in this file
         self.provider = IBMProvider()
+
         if not self.provider.active_account():
             raise EnvironmentError("Can't find the account saved in this session. Please activate in the script folder")
+        self._available_backends: List[IBMBackend] = []
         self._set_up()
 
     def _set_up(self) -> None:
@@ -24,13 +26,13 @@ class BackEndList(DefaultDict[str, int]):
         Get the name from each backend
         :return:
         """
-        backends = self.provider.backends()
-        for backend in backends:
+        self._available_backends = self.provider.backends()
+        for backend in self._available_backends:
             # We set it all to 0 except nairobi backend
             if "nairobi" in str(backend_name := backend.name):
-                self[backend_name] = QUBIT_PARA.NUM_QUBIT_TYPE2.value
+                self[backend_name] = backend, QUBIT_PARA.NUM_QUBIT_TYPE2.value
             else:
-                self[backend_name] = QUBIT_PARA.NUM_QUBIT_TYPE1.value
+                self[backend_name] = backend, QUBIT_PARA.NUM_QUBIT_TYPE1.value
 
     def show(self) -> None:
         """
@@ -39,12 +41,12 @@ class BackEndList(DefaultDict[str, int]):
         """
         print(f"{'Backend name:':<30}{'# Qubit used:':<40}")
         for name in self:
-            print(f"{name:<30}{self[name]:<40}")
+            print(f"{name:<30}{self[name][1]:<40}")
 
-    def default_backend(self, quantum_computer: str = 'ibm_nairobi') -> Tuple[Backend, int]:
+    def default_backend(self, quantum_computer: str = 'ibm_nairobi') -> Tuple[IBMBackend, int]:
         """
         Return nairobi backend as the default and its qubit value
         :return:
         """
-        backend = self.provider.get_backend(quantum_computer)
+        backend = self[quantum_computer][0]
         return backend, self[quantum_computer]
