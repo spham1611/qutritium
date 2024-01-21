1f380f12797d0fb90ca005f3fdc19b89836a47d5
Son Pham <reachphamhson@gmail.com>
2024-01-21 17:03:46 +0700

    Publish package

--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
-import setuptools
+from setuptools import setup, find_packages
 
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="qutritium",
     version="0.0.1",
-    author="Son Pham, Tien Nguyen",
-    author_email="reachphamhson@gmail.com, nguyendinhtien_t65@hus.edu.vn",
+    author="Son Pham, Tien Nguyen, Bao Bach",
+    author_email="reachphamhson@gmail.com",
     description="Qutritium package provides basic calibration and features for qutrit system using Qiskit and other "
                 "quantum packages",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license='MIT License',
     url="https://github.com/spham1611/qutritium",
-    project_urls={
-        'Bug Tracker: https://github.com/spham1611/qutritium/~/issues',
-        'repository: https://github.com/spham1611/qutritium'
-    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Utilities",
     ],
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    packages=find_packages(where="src"),
     python_requires=">=3.6"
 )
+
