33ca4582d6dddad5e2864a9879657d8505db7935
spham1611 <shp40@duke.edu>
2026-05-25 11:26:00 +0700

    - Add CPHASE

--- src/qutritium/gates/__init__.py
+++ src/qutritium/gates/__init__.py
@@ -7,7 +7,7 @@ from qutritium.gates.base import Gate
 from qutritium.gates.single_qutrit import (G01, G02, G12, H3, I3, Rx01, Rx02, Rx12, Ry01, Ry02, Ry12, Rz01, Rz02, Rz12,
                                            S3, T3, Ud, UFT, X01, X02, X12, XMinus, XPlus, Y01, Y02, Y12, Z01, Z02, Z12)
 # Two-qutrit gates
-from qutritium.gates.two_qutrit import (CNOT3, CPhase, CSUM, CSUMDag, SWAP3)
+from qutritium.gates.two_qutrit import (CNOT3, CPhase, CPhaseDag, CSUM, CSUMDag, SWAP3)
 
 __all__ = [
     # Base class
@@ -27,6 +27,6 @@ __all__ = [
     # Two-qutrit gates
     "CSUM", "CSUMDag",
     "CNOT3",
-    "CPhase",
+    "CPhase", "CPhaseDag",
     "SWAP3",
 ]
